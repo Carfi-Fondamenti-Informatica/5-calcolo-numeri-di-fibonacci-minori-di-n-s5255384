60469a181c49830be6a45fca5ac9a36e02c6b047
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-04-05 20:03:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+    int a=0, b=1, c=0, n=0, i;
+    cout<<"inserisci quante n volte sviluppare la serie di fibonacci."<<endl;
+    cin>>n;
+
+    cout<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+    while(c<n) {
+        c=a+b;
+        cout<<c<<endl;
+        a=b;
+        b=c;
+    }
    return 0;
 }
